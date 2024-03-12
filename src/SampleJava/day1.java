f6e08ccfae880f6f0bb0b1e71870e035cbb914f5
Bhagat Pyakurel <bhagatpyakurel@Bhagats-MacBook-Pro.local>
2024-03-11 23:03:57 -0400

    Change has maid

--- src/SampleJava/day1.java
+++ src/SampleJava/day1.java
@@ -5,7 +5,9 @@ public class day1 {
 	public static void main(String[] args) {
 		
 
-		System.out.println("good evening");
+		System.out.println("good Morning");
+		System.out.println("Good AfterNoon");
+		
 	}
 
 }
